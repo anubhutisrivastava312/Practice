863b737ec6e7b0b197778a2de45ff707ab142d1d
Anubhuti Srivastava <anubhutisrivastava312@gmail.com>
2022-10-07 14:18:33 +0530

    added a program to check anagram strings

--- String_programs.py
+++ String_programs.py
@@ -9,10 +9,27 @@
 
 
 #program to count the occurrence of acharacter in the given string
-string=input(" enter the string: ")
-character=input(" enter the character to be counted: ")
-count=0
-for i in range(len(string)):
-    if string[i]==character:
-        count=count+1
-print("the number is :", count)        
+# string=input(" enter the string: ")
+# character=input(" enter the character to be counted: ")
+# count=0
+# for i in range(len(string)):
+#     if string[i]==character:
+#         count=count+1
+# print("the number is :", count)      
+
+
+
+
+#program to check whether the given two strings are anagram or not
+def anagram(str1,str2):
+    if sorted(str1)==sorted(str2):
+        return True
+    else:
+        return False
+
+str1=input(" enter the string1: ")
+str2=input(" enter the string 2: ")   
+if anagram(str1,str2):
+    print("Anagram!")
+else:
+    print("not anagram!")             
