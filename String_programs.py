c6ca0419b255ff6fc00a2a845297b7047b46d415
Anubhuti Srivastava <anubhutisrivastava312@gmail.com>
2022-10-07 14:02:34 +0530

    added a program

--- String_programs.py
+++ String_programs.py
@@ -1,7 +1,18 @@
 #program to remove a character in the given string
-def remove_char(str1,str2):
-    print(str1.replace(str2," "))
+# def remove_char(str1,str2):
+#     print(str1.replace(str2," "))
 
-str1=input("enter the string: ")
-str2=input(" enter the character which has to be removed from the string:")
-remove_char(str1,str2)    
+# str1=input("enter the string: ")
+# str2=input(" enter the character which has to be removed from the string:")
+# remove_char(str1,str2)    
+
+
+
+#program to count the occurrence of acharacter in the given string
+string=input(" enter the string: ")
+character=input(" enter the character to be counted: ")
+count=0
+for i in range(len(string)):
+    if string[i]==character:
+        count=count+1
+print("the number is :", count)        
